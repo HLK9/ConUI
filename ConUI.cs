ea074ec832857e4a4e58747201471837745e839e
Hoang Long <62252259+HLK9@users.noreply.github.com>
2022-08-06 15:02:10 +0700

    Create ConUI.cs

---
+++ ConUI.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin.Controls;
+using System.Diagnostics;
+
+namespace ConUI
+{
+    public partial class ConUI : MaterialForm
+    {
+        public ConUI()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnRunCus_Click(object sender, EventArgs e)
+        {
+            Process runCustom = new Process();
+            runCustom.StartInfo.FileName = "cmd.exe";
+            runCustom.StartInfo.Arguments = "/c " + txtCmdCus.Text;
+            runCustom.StartInfo.RedirectStandardOutput = true;
+            runCustom.StartInfo.UseShellExecute = false;
+            runCustom.StartInfo.CreateNoWindow = true;
+            runCustom.Start();
+            runCustom.WaitForExit();
+            richOutput.Text += "\n"+runCustom.StandardOutput.ReadLine();
+
+        }
+    }
+}
